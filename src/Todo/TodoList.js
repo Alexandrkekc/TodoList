a00e0f72f30d80ad61b76efd4af008810f56cf85
Maksim2806 <maksimpolezhaev22@gmail.com>
2021-05-05 17:28:54 +0400

    modified app.js Todolist.js package.json, created Todoitem.js

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import TodoItem from './TodoItem'
 
-export default function TodoList() {
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
+const styles = {
+    ul: {
+        listStyle: 'none',
+        margin: 0,
+        padding: 0
+    }
+}
+
+export default function TodoList(props) {
+    return (        
+        <ul style={styles.ul}>
+            {props.todos.map((todo, i) => {
+              return (<TodoItem todo={todo} key={todo.id} index={i} onChange={props.onToggle} />)
+            })}
         </ul>
     )
 }
