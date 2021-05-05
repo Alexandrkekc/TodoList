0bb815709acceee363e9d0bbf9f56839359a0087
Maksim2806 <maksimpolezhaev22@gmail.com>
2021-05-05 19:07:21 +0400

    modified

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react'
 import TodoList from './Todo/TodoList'
 
 function App() {
-  const todos = [
+  let todos = [
     {id: 1, completed: false, title: 'Купить хлеб'},
     {id: 2, completed: false, title: 'Купить молоко'},
     {id: 3, completed: false, title: 'Купить масло'}
   ]
 
 function toggleTodo(id) {
-  console.log('todo id', id)
+  todos = todos.map(todo => {
+    if (todo.id === id) {
+      todo.completed = !todo.completed
+    } return todo
+  })
 }
 
 
--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import TodoItem from './TodoItem'
+import React from "react";
+import TodoItem from "./TodoItem";
 
 const styles = {
-    ul: {
-        listStyle: 'none',
-        margin: 0,
-        padding: 0
-    }
-}
+  ul: {
+    listStyle: "none",
+    margin: 0,
+    padding: 0,
+  },
+};
 
 export default function TodoList(props) {
-    return (        
-        <ul style={styles.ul}>
-            {props.todos.map((todo, i) => {
-              return (<TodoItem todo={todo} key={todo.id} index={i} onChange={props.onToggle} />)
-            })}
-        </ul>
-    )
+  return (
+    <ul style={styles.ul}>
+      {props.todos.map((todo, i) => {
+        return (
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            index={i}
+            onChange={props.onToggle}
+          />
+        );
+      })}
+    </ul>
+  );
 }
-
